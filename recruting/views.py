71e72d5d042a1653fba71bb98cd0db6343afa0cf
nikitosik <nikitadanko2002@gmail.com>
2024-08-28 16:24:13 +0300

    first

---
+++ recruting/views.py
@@ -0,0 +1,8 @@
+# hello/views.py
+from django.http import HttpResponse
+
+def hello_recruto(request):
+    name = request.GET.get('name', 'Recruto')
+    message = request.GET.get('message', 'Давай дружить')
+    response_text = f"Hello {name}! {message}!"
+    return HttpResponse(response_text)
