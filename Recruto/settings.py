d4689de121d89d7768a2ed8b1944d508411a387c
nikitosik <nikitadanko2002@gmail.com>
2024-08-29 17:18:25 +0300

    fixedddddвввв

--- Recruto/settings.py
+++ Recruto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-%g)hykw134d79=-lvq0aptqhe+afruj)he#mcoc9*jlg(gb=*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['recruto-69de.onrender.com']
 
 
 # Application definition
